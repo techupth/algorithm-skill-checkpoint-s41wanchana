527be63ddd0f4949e7ed07ee3632c46671e92f97
Wanchana <s.wanchana41@gmail.com>
2024-07-30 10:28:43 +0700

    fix: answer for ex5

--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer:
+Answer: O(n^2) because the function have a loop inside a loop and each loop take n step to complete, therefore the total step needed to complete the function is n*n = n^2 = O(n^2).
 
 
 */
